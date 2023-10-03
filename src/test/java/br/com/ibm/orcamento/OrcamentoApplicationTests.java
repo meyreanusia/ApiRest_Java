081bd2f63b90cf4a2e8f85db1021f2ab7f880661
Meyre Anusia <meyreanusia@gmail.com>
2023-10-03 20:28:43 -0300

    Porjeto inicial

---
+++ src/test/java/br/com/ibm/orcamento/OrcamentoApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.ibm.orcamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
