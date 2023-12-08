0f72e3f36c7d70263369a077c8ecfc08e3dde891
igabriel39 <gabriel.mendonca2@hotmail.com>
2023-12-08 09:38:57 -0300

    corrige allowedOrigins

--- src/main/java/br/com/ibm/orcamento/config/WebConfig.java
+++ src/main/java/br/com/ibm/orcamento/config/WebConfig.java
@@ -9,9 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry)
     {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);;
     }
 }
